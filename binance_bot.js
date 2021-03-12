17d6f58a1a8e9d7247cf6a0861ebe889d9a22f5f
ilker <19080242+ilker1996@users.noreply.github.com>
2021-03-12 17:25:37 +0300

    Test spot trading and trade directly from market

--- binance_bot.js
+++ binance_bot.js
@@ -3,35 +3,44 @@ const BinanceTrader = require('node-binance-api');
 
 const { EMA } = require('technicalindicators');
 
-// Creates the API caller/requester as an authenticated client, which can make signed calls
-const BINANCE_API_KEY = require("./binance_secrets.json");
+const trade_type = {
+	SPOT: "spot",
+	FUTURE: "future",
+}
 
-const binanceServer = BinanceServer({
+const session_type = {
+	TEST: "test",
+	TRADE: "trade",
+}
+
+const SESSION_TYPE = session_type.TEST;
+const TRADE_TYPE = trade_type.SPOT;
+
+
+let BINANCE_API_KEY = {
+    api_key: "test",
+    api_secret: "test"
+}
+
+if(SESSION_TYPE == session_type.TRADE) {
+	BINANCE_API_KEY = require("./binance_secrets.json");
+} 
+
+// Creates the API caller/requester as an authenticated client, which can make signed calls
+
+const binanceServer = BinanceServer({ 
 	apiKey: BINANCE_API_KEY.api_key,
-	apiSecret: BINANCE_API_KEY.api_secret,
+	APISECRET: BINANCE_API_KEY.api_secret,
 });
 
 const binanceTrader = new BinanceTrader().options({
 	APIKEY: BINANCE_API_KEY.api_key,
-	APISECRET: BINANCE_API_KEY.api_secret
+	apiKey: BINANCE_API_KEY.api_secret
 });
 
 const COIN_PAIR = 'BANDUSDT';
 const CANDLE_INTERVAL = '15m';
 const TRADING_CURRENCY = 'USDT';
-const WAITING_TIME_MS = 1000 * 60 * 15; // 15 minutes
-
-// VARIABLES - Binance API
-let buyOrderInfo = null;
-let sellOrderInfo = null;
-
-const INDEX_USDT = 11;
-const PRICE_UPDATE_PERIOD = 5000; // Price update times varies a lot
-const ORDER_UPDATE_PERIOD = 3000;
-
-const BUY_LIMIT = 5; 
-
-// FUNCTIONS
 
 // PROTOTYPE FUNCTIONS
 if (!Array.prototype.last){
@@ -53,6 +62,8 @@ if (!Array.prototype.subtract){
     };
 };
 
+// FUNCTIONS
+
 // Pauses execution for a specified amount of time
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -64,7 +75,7 @@ const sync = async () => {
 }
 
 // Adjust the input for the EMA calculation
-const fetch_initial_candles = async (symbol, interval, limit = 200) => {
+const fetch_initial_candles = async (symbol, interval) => {
 	console.log('FETCHING INITIAL CANDLES...');
 	
 	let candles = [];
@@ -89,7 +100,8 @@ const fetch_initial_candles = async (symbol, interval, limit = 200) => {
 		},
 	}
 
-	const start = candles.length > limit ? candles.length - limit : 0;
+	// const start = candles.length > limit ? candles.length - limit : 0;
+	const start = 0;
 
 	for(let i = start; i < candles.length; ++i) {
 		newCandles.opening.values[i - start] = Number(candles[i].open);
@@ -102,26 +114,30 @@ const fetch_initial_candles = async (symbol, interval, limit = 200) => {
 	return newCandles;
 }
 
-const calculateEMADiff = async (openingPrices, closingPrices) => {
+// Calculate ema1 and ema2
+const calculateEMAs = (openingPrices, closingPrices) => {
 	const ema1 = EMA.calculate({period: 13, values: closingPrices});
 	const ema2 = EMA.calculate({period: 21, values: openingPrices});
 
-	const result = ema2.last() - ema1.last();
-
-	return result;
+	return {
+		ema1 : ema1.last(),
+		ema2 : ema2.last()
+	}
 }
 
 // Calculates how much of the asset(coin) the program can buy.
-const calculateBuyQuantity = async (symbol, trading_currency="USDT") => {
+const calculateBuyQuantity = async (symbol, trading_currency="USDT", test=true) => {
 	console.log('CALCULATING BUY QUANTITY...');
 
-	const accountInfo = await binanceServer.accountInfo();
-	const prices = await binanceServer.prices();
-
-	const free_balance = parseFloat(accountInfo.balances.find(b => b.asset === trading_currency).free);
-	const buying_balance = free_balance > 20 ? 20 : free_balance;
+	let buying_balance = 20;
+	if(!test) {
+		const accountInfo = await binanceServer.accountInfo();
+		const free_balance = parseFloat(accountInfo.balances.find(b => b.asset === trading_currency).free);
+		buying_balance = free_balance > buying_balance ? buying_balance : free_balance;
+	}
 	
-	const coin_price = parseFloat(prices[symbol]) - 0.5;
+	const prices = await binanceServer.prices();
+	const coin_price = parseFloat(prices[symbol]);
 	
 	const quantity = buying_balance / coin_price; 
 
@@ -131,176 +147,7 @@ const calculateBuyQuantity = async (symbol, trading_currency="USDT") => {
 	};
 }
 
-// Creates a buy order in the Binance API
-const makeBuyOrder = async ({quantity, price} = {}) => {
-	console.log('MAKING BUY ORDER');
-	
-	const buyOrderInfo = await binanceServer.order({
-		symbol: COIN_PAIR,
-		side: 'BUY',
-		quantity: quantity.toString(),
-    	price: price.toString()
-	});
-
-	return buyOrderInfo;
-}
-
-// Waits till a buy order is completely filled or times out empty
-const waitBuyOrderCompletion = async () => {
-	console.log('WAITING BUY ORDER COMPLETION');
-
-	for(let i = 0; i < 5;	i++){
-		buyOrderInfo = await client.getOrder({
-			symbol: COIN_PAIR,
-  			orderId: buyOrderInfo.orderId,
-		});
-
-		if(buyOrderInfo.status === 'FILLED'){
-			console.log('PURCHASE COMPLETED! \n');
-			return 'success';
-		}
-
-		await wait(ORDER_UPDATE_PERIOD);
-	}
-	
-	if(buyOrderInfo.status === 'PARTIALLY_FILLED'){
-
-		console.log('PURCHASE PARTIALLY FILLED, CONTINUING');
-
-		while(true){
-			buyOrderInfo = await client.getOrder({
-				symbol: COIN_PAIR,
-				orderId: buyOrderInfo.orderId,
-			});
-
-			if(buyOrderInfo.status === 'FILLED'){
-
-				console.log('PURCHASE COMPLETED! \n');
-				return 'success';
-			}
-
-			await wait(ORDER_UPDATE_PERIOD);
-		}
-	}
-
-	console.log('PURCHASE TIMED OUT, CANCELLING \n');
-
-	await client.cancelOrder({
-		symbol: COIN_PAIR,
-  		orderId: buyOrderInfo.orderId,
-	});
-
-	return 'failure';
-}
-
-// Purchasing mechanism, invokes the 3 functions above as needed
-const buy = async () => {
-	console.log('BUYING');
-
-	const { buyQuantity, currentPrice } = await calculateBuyQuantity();
-	await makeBuyOrder(buyQuantity, currentPrice);
-
-	const buySuccess = await waitBuyOrderCompletion();
-	return buySuccess;
-}
-
-// Calculates how much profit a sale would incur
-const calculateProfit = async () => {
-	console.log('CALCULATING PROFIT');
-	
-	const buyingPrice = buyOrderInfo.price;
-	const prices = await client.prices({ symbol: COIN_PAIR });
-
-	const currentPrice = prices.XRPUSDT;
-	const profit = ((currentPrice / buyingPrice) - 1) * 100;
-
-	console.log('profit: ', profit, '\n');
-
-	return {
-		profit,
-		currentPrice
-	};
-}
-
-// Creates a sell order in the Binance API
-const makeSellOrder = async (currentPrice) => {
-	console.log('MAKING SELL ORDER');
-
-	sellOrderInfo = await client.order({
-		symbol: COIN_PAIR,
-		side: 'SELL',
-		quantity: buyOrderInfo.executedQty,
-		price: currentPrice,
-	});
-
-	console.log('sellOrderInfo: ', sellOrderInfo, '\n');
-}
-
-// Waits till a sell order is completely filled or times out empty
-const waitSellOrderCompletion = async () => {
-	console.log('WAITING SELL ORDER COMPLETION');
-
-	for(let i = 0; i < 5; i++){
-		sellOrderInfo = await client.getOrder({
-			symbol: COIN_PAIR,
-			orderId: sellOrderInfo.orderId,
-		});
-
-		if(sellOrderInfo.status === 'FILLED'){
-			console.log('SALE COMPLETE! \n');
-			return 'success';
-		}
-
-		await wait(ORDER_UPDATE_PERIOD);
-	}
-
-	if(sellOrderInfo.status === 'PARTIALLY_FILLED'){
-
-		console.log('SALE PARTIALLY FILLED, CONTINUING');
-
-		while(true){
-			sellOrderInfo = await client.getOrder({
-				symbol: COIN_PAIR,
-				orderId: sellOrderInfo.orderId,
-			});
-
-			if(sellOrderInfo.status === 'FILLED'){
-				console.log('SALE COMPLETE! \n');
-				return 'success';
-			}
-
-			await wait(ORDER_UPDATE_PERIOD);
-		}
-	}
-
-	console.log('SALE TIMED OUT, CANCELLING \n');
-
-	await client.cancelOrder({
-		symbol: COIN_PAIR,
-  		orderId: sellOrderInfo.orderId,
-	});
-
-	return 'failure';
-}
-
-// Selling mechanism, invokes the 3 functions above as needed
-const sell = async () => {
-	console.log('SELLING');
-
-	while(true){
-		const { profit, currentPrice } = await calculateProfit();
-
-		if(profit >= 0.175){
-			await makeSellOrder(currentPrice);
-			const sellSuccess = await waitSellOrderCompletion();
-			if(sellSuccess === 'failure') continue;
-			return;
-		}
-
-		await wait(PRICE_UPDATE_PERIOD);
-	}
-}
-
+// Add lastest candle to the list
 const add_candle = (candles, latest_candle) => {
 	candles.opening.values.shift();
 	candles.opening.times.shift();
@@ -313,109 +160,213 @@ const add_candle = (candles, latest_candle) => {
 	candles.closing.times.push(latest_candle.closeTime);
 }
 
-const trade = async (symbol, trading_currency="USDT") => {
-	const { expected_quantity, expected_price} = await calculateBuyQuantity(symbol, trading_currency);
+// Track price for spot trading
+const track_spot_price = async (symbol, {stop_loss_order_id, stop_price, quantity, buying_price}, test=true) => {
+	const expected_profit_amount = buying_price * 0.1;
+	
+	let higher_selling_price = buying_price + expected_profit_amount;
+	let current_price = buying_price;
+	let selling_price = stop_price;
 
-	// MARKET BUY
-	binanceTrader.marketBuy(symbol, expected_quantity, (error, response) => {
-		if(error) {
-			console.log("Error occured during Market Buy", error.body);
-		} else {
-			// SAMPLE RESPONSE
-			// {
-			// 	symbol: 'OCEANUSDT',
-			// 	orderId: 812125125941,
-			// 	orderListId: -1,
-			// 	clientOrderId: 'ag8ashgkashash88128IKHJS',
-			// 	transactTime: 82196816816,
-			// 	price: '0.00000000',
-			// 	origQty: '8.00000000',
-			// 	executedQty: '8.00000000',
-			// 	cummulativeQuoteQty: '10.69200000',
-			// 	status: 'FILLED',
-			// 	timeInForce: 'GTC',
-			// 	type: 'MARKET',
-			// 	side: 'BUY',
-			// 	fills: [
-			// 	  {
-			// 		price: '1.33650000',
-			// 		qty: '8.00000000',
-			// 		commission: '0.00800000',
-			// 		commissionAsset: 'OCEAN',
-			// 		tradeId: 86138128
-			// 	  }
-			// 	]
-			// }
+	let sold = false;
 
-			const orderId = response.orderId;
-			const { 
-				price: buying_price,
-				qty: buying_quantity,
-			} = response?.fills[0];
-
-			const actual_buying_price = buying_price || expected_price ;
-			const actual_quantity = buying_quantity || expected_quantity ;
-
-			const stop_price = actual_buying_price * 0.99;
-
-			// PLACE STOP_LOSS order
-			binanceTrader.sell(symbol, actual_quantity, stop_price, {stopPrice: stop_price, type: "STOP_LOSS_LIMIT"}, (error, response) => {
-				if (error) {
-					console.log("Error occured during stop-loss order", error.body);
-				} else {
-					// SAMPLE RESPONSE
-					// {
-					// 	symbol: 'OCEANUSDT',
-					// 	orderId: 831212161621,
-					// 	orderListId: -1,
-					// 	clientOrderId: '43asgsf8s0IwtkQGMsgazM',
-					// 	transactTime: 16154252152270
-					// }
+	while(!sold) {
+		const prices = await binanceServer.prices();
+		current_price = parseFloat(prices[symbol]);
+		console.log("Price of the : ", symbol, " : ", current_price);
 		
-					const orderId = response.orderId;
-					console.log("Stop-Loss order response", response);
-				}
-			});
+		if(current_price >= higher_selling_price) {
+			selling_price = higher_selling_price;
+			higher_selling_price += expected_profit;
 
-			// PLACE SELL ORDER AT %1 PROFIT UP FOR HALF QUANTITY
-			// TRAIL STOP-LOSS FOR OTHER HALF QUANTITY
+		} else if(current_price <= selling_price) {
+			selling_price = current_price;
+			
+			if(test) {
+				console.log("SOLD ", symbol, " QUANTITY : ", quantity, " PRICE : ", selling_price);
+				sold = true;
+			} else {
+				binanceTrader.marketSell(symbol, quantity, (error, response) => {
+					if(error) {
+						console.log("Error occured during Market Sell", error.body);
+					} else if(response) {	
+						// SAMPLE RESPONSE ?
+						// {
+						// 	symbol: 'OCEANUSDT',
+						// 	orderId: 812125125941,
+						// 	orderListId: -1,
+						// 	clientOrderId: 'ag8ashgkashash88128IKHJS',
+						// 	transactTime: 82196816816,
+						// 	price: '0.00000000',
+						// 	origQty: '8.00000000',
+						// 	executedQty: '8.00000000',
+						// 	cummulativeQuoteQty: '10.69200000',
+						// 	status: 'FILLED',
+						// 	timeInForce: 'GTC',
+						// 	type: 'MARKET',
+						// 	side: 'BUY',
+						// 	fills: [
+						// 	  {
+						// 		price: '1.33650000',
+						// 		qty: '8.00000000',
+						// 		commission: '0.00800000',
+						// 		commissionAsset: 'OCEAN',
+						// 		tradeId: 86138128
+						// 	  }
+						// 	]
+						// }
+
+						// const { 
+						// 	price: selling_price,
+						// 	qty: selling_quantity,
+						// } = response.fills[0];
+
+						console.log("SOLD ", symbol, " QUANTITY : ", quantity, " PRICE : ", selling_price);
+
+						sold = true;
+					}
+				});
+			}
 		}
-	});
+	}
+
+	const profit = selling_price - buying_price;
+	console.log("PROFIT is: ", profit);
+
+	return profit;
+} 
+
+// Start spot trading
+const spot_trade = async (symbol, trading_currency="USDT", test=true) => {
+	const { expected_quantity, expected_price} = await calculateBuyQuantity(symbol, trading_currency, test);
+	let profit = 0;
+
+	if(test) {
+		console.log("BOUGHT ", symbol, "AT PRICE : ", expected_price);
+
+		const stop_price = expected_price * 0.99;
+		const trade_info = {
+			stop_loss_order_id: 5,
+			stop_price: stop_price,
+			quantity: expected_quantity,
+			buying_price: expected_price
+		};
+
+		profit = track_spot_price(symbol, trade_info, test);
+	} else {
+		// MARKET BUY
+		binanceTrader.marketBuy(symbol, expected_quantity, (error, response) => {
+			if(error) {
+				console.log("Error occured during Market Buy", error.body);
+			} else if(response) {
+				// SAMPLE RESPONSE
+				// {
+				// 	symbol: 'OCEANUSDT',
+				// 	orderId: 812125125941,
+				// 	orderListId: -1,
+				// 	clientOrderId: 'ag8ashgkashash88128IKHJS',
+				// 	transactTime: 82196816816,
+				// 	price: '0.00000000',
+				// 	origQty: '8.00000000',
+				// 	executedQty: '8.00000000',
+				// 	cummulativeQuoteQty: '10.69200000',
+				// 	status: 'FILLED',
+				// 	timeInForce: 'GTC',
+				// 	type: 'MARKET',
+				// 	side: 'BUY',
+				// 	fills: [
+				// 	  {
+				// 		price: '1.33650000',
+				// 		qty: '8.00000000',
+				// 		commission: '0.00800000',
+				// 		commissionAsset: 'OCEAN',
+				// 		tradeId: 86138128
+				// 	  }
+				// 	]
+				// }
+
+				const { 
+					price: buying_price,
+					qty: buying_quantity,
+				} = response.fills[0];
+
+				const actual_buying_price = buying_price || expected_price ;
+				const actual_quantity = buying_quantity || expected_quantity ;
+
+				const stop_price = actual_buying_price * 0.99;
+				const trade_info = {
+					stop_loss_order_id: 5,
+					stop_price: stop_price,
+					quantity: actual_quantity,
+					buying_price: actual_buying_price
+				};
+
+				profit = track_spot_price(symbol, trade_info, test);
+			}
+		});
+	}
+
+	return profit;
+}
+
+// Start future trading
+const future_trade = async (symbol, trading_currency="USDT") => {
+	if(SESSION_TYPE == session_type.TEST) {
+		console.log("Future testing is not implemented");
+	} else {
+		console.log("Future trading is not implemented!");
+	}
+	
 }
 
 // Main function, entrance point for the program
 const start = async (symbol, interval) => {
-	const candles = await fetch_initial_candles(symbol, interval, 400);
-	let prev_ema_diff = calculateEMADiff(candles.opening.values, candles.closing.values);
+	const candles = await fetch_initial_candles(symbol, interval);
+
+	let total_profit = 0;
+	let prev_emas = {ema1: 0, ema2: 0};
+	let curr_emas = {ema1: 0, ema2: 0};
+	
+	prev_emas = calculateEMAs(candles.opening.values, candles.closing.values);
+
+	let trade_profit = 0;
+	if(TRADE_TYPE == trade_type.SPOT) {
+		trade_profit = await spot_trade(COIN_PAIR, TRADING_CURRENCY, true);			
+	} else if(TRADE_TYPE == trade_type.FUTURE) {
+		trade_profit = await future_trade(COIN_PAIR, TRADING_CURRENCY, true);
+	}
 
 	binanceServer.ws.candles(symbol, interval, tick => {
-		const {
-			open: open, 
-			close: close
-		} = tick;
-
-		console.log("FETCHING CURRENT PRICE FOR ", symbol, ": open", tick.open, "close", tick.close);
-		const openingPrices = candles.opening.values.concat(open);
-		const closingPrices = candles.closing.values.concat(close);
+		const openingPrices = candles.opening.values.concat(tick.open);
+		const closingPrices = candles.closing.values.concat(tick.close);
 		openingPrices.shift();
 		closingPrices.shift();
-		const curr_ema_diff = calculateEMADiff(openingPrices, closingPrices);
-		
-		if(prev_ema_diff > 0 && curr_ema_diff <= 0){
-			console.log("PREVIOUS DIFFERENCE : ", prev_ema_diff, "CURRENT DIFFERENCE : ", curr_ema_diff);
 
+		curr_emas = calculateEMAs(openingPrices, closingPrices);
+		console.log("CURRENT EMA1 : ", curr_emas.ema1, "EMA2", curr_emas.ema2);
+
+		if(prev_emas.ema2 > prev_emas.ema1 && curr_emas.ema2 <= curr_emas.ema1) {
 			const time = new Date(tick.eventTime);
-			console.log("ALIM FIRSATI : ", time.toLocaleTimeString());
+			console.log("START TRADING FOR ", symbol, " AT ", time.toLocaleTimeString());
 			
-			// START TRADING THE COIN
-			trade(COIN_PAIR, TRADING_CURRENCY);
+			// START TRADING
+			let trade_profit = 0;
+			if(TRADE_TYPE == trade_type.SPOT) {
+				trade_profit = await spot_trade(COIN_PAIR, TRADING_CURRENCY, true);			
+			} else if(TRADE_TYPE == trade_type.FUTURE) {
+				trade_profit = await future_trade(COIN_PAIR, TRADING_CURRENCY, true);
+			}
+			console.log("PROFIT FROM THE TRADE IS : ", trade_profit);
+
+			total_profit += trade_profit;
+			console.log("TOTAL PROFIT IS : ", total_profit);
 		}
 
 		if(tick.isFinal) {
 			add_candle(candles, tick);
-			prev_ema_diff = calculateEMADiff(candles.opening.values, candles.closing.values);
+			prev_emas = curr_emas;
 		}
 	});
 };
 
-// start(COIN_PAIR, CANDLE_INTERVAL);
+start(COIN_PAIR, CANDLE_INTERVAL);
