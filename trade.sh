e7c57aeb36881d39f193ff1bc4daf4c686a2a610
ilker <19080242+ilker1996@users.noreply.github.com>
2021-03-13 14:55:54 +0300

    Give argument for ouput dir

--- trade.sh
+++ trade.sh
@@ -15,11 +15,11 @@ YFII \
 )
 
 # The .txt files' directory for test results
-output_directory=./spot_ema_scalper
+output_directory=./${1}
 mkdir -p ${output_directory}
 
 for coin in ${coin_list[@]};
 do
     pair=${coin}${currency}
 	ttab node binance_bot.js ${pair} > ${output_directory}/${pair}.txt
-done;
+done
