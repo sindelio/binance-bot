d887981a730b3b89b0483e461f97740eca0fa419
ilker <19080242+ilker1996@users.noreply.github.com>
2021-03-15 16:37:13 +0300

    Log ema values when indicator singals

--- indicators.js
+++ indicators.js
@@ -8,7 +8,10 @@ exports.ema_scalper = (open_prices, close_prices) => {
 	const [prev_ema2, curr_ema2] = EMA.calculate({period: 21, values: open_prices}).slice(-2).map(precise);
 	
 	const signal = (prev_ema2 > prev_ema1) && (curr_ema2 <= curr_ema1);
-	
+	if(signal) {
+		console.log("current ema1 :", curr_ema1, "current ema2 :", curr_ema2);
+		console.log("previous ema1 :", prev_ema1, "previous ema2 :", prev_ema2);
+	}
 	return signal;
 }
 
