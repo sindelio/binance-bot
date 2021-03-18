df45594c445f9b7946c524acd16d9941223032f0
ilker <19080242+ilker1996@users.noreply.github.com>
2021-03-18 17:41:45 +0300

    USe either one of the scalpers

--- indicators.js
+++ indicators.js
@@ -12,8 +12,8 @@ const ema_scalper = (open_prices, close_prices, price_digit=4, logger) => {
 	const [prev_ema13, curr_ema13] = EMA.calculate({period: 13, values: close_prices}).slice(-2).map(precise);
 	const [prev_ema21, curr_ema21] = EMA.calculate({period: 21, values: open_prices}).slice(-2).map(precise);
 
-	const signal = (curr_ema6 > curr_ema12) && (prev_ema6 <= prev_ema12) && (prev_ema13 <= prev_ema21) ||
-					(curr_ema13 > curr_ema21) && (prev_ema13 <= prev_ema21) && (prev_ema6 >= prev_ema12);
+	const signal = (curr_ema6 > curr_ema12) && (prev_ema6 <= prev_ema12)  ||
+					(curr_ema13 > curr_ema21) && (prev_ema13 <= prev_ema21);
 	
 	if(signal) {
 		logger.info("current ema21 : %f and current ema13 : %f", curr_ema21, curr_ema13);
